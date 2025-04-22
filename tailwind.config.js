10be4b19ab248124e794831ce6f9855f11f1d421
Gabriel Almeida Dias <bielalmeidadias@hotmail.com>
2025-04-22 08:42:23 -0300

    Inicializando o projeto

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./app/**/*.jsx",
+    "./pages/**/*.jsx",
+    "./components/**/*.jsx",
+    "./src/**/*.jsx"
+  ],
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
